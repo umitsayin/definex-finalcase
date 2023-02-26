cff7ce094bbce8fc065a1581e7ae29b16bb16969
Ümit Sayın <umitsayin1661@gmail.com>
2023-02-27 00:29:02 +0300

    definex-end

--- src/main/java/com/definex/config/SecurityConstant.java
+++ src/main/java/com/definex/config/SecurityConstant.java
@@ -4,7 +4,7 @@ public final class SecurityConstant {
 
     private SecurityConstant(){}
 
-    public static final String SECRET_KEY = "WORD_APP";
+    public static final String SECRET_KEY = "DEFINEX";
     public static final int CURRENT_EXPIRED_DAY = 5;
     public static final String AUTHORIZATION_TOKEN_PREFIX = "Bearer ";
 
